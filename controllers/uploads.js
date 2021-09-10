3563f09c822a5c3f6da98945b42df111557918d9
Christian Guerra <caguerrao@gmail.com>
2021-09-10 15:31:23 -0300

    Subiendo archivo 50perc

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,13 +1,67 @@
-const { response } = require("express");
+const { response, json } = require("express");
+const { subirArchivo } = require("../helpers/subirArchivo");
+const {Usuario, Producto } = require('../models')
 
-const cargarArchivo = (req, res = response) => { 
+const cargarArchivo = async (req, res = response) => {
+  if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
+    res.status(400).send("No hay archivos que subir.");
+    return;
+  }
 
-    res.json({
-        MSG: 'hOLA mUNDO'
-    })
+  try {
+    const nombre = await subirArchivo(req.files, undefined, 'varios');
+    res.json({ nombre });
+  } catch (msg) {
+    res.status(400).json({ msg });
+  }
+};
+
+
+const actualizarImagen = async ( req, res = response)=> {
+
+  const { id, coleccion } = req.params;
+  
+  let modelo;
+
+  switch ( coleccion) {
+
+    case 'usuarios':
+      modelo = await Usuario.findById(id);
+      if ( !modelo ){
+        return res.status(400).json({
+          msg: `No existe un usuario con el id ${ id }`
+        })
+      }      
+      break;
+
+      case 'productos':
+      modelo = await Producto.findById(id);
+      if ( !modelo ){
+        return res.status(400).json({
+          msg: `No existe un producto con el id ${ id }`
+        })
+      }
+
+
+      
+      break;
+  
+    default:
+
+    return res.status(500).json ({ msg: 'Se me olvido validar esto'});
+      break;
+  }
+
+  const nombre = await subirArchivo(req.files, undefined, coleccion);
+  modelo.img = nombre;
+
+  await modelo.save();
+
+  return json({modelo});
 
 }
 
 module.exports = {
-    cargarArchivo
-}
+  cargarArchivo,
+  actualizarImagen
+};
--- routes/uploads.js
+++ routes/uploads.js
@@ -1,10 +1,17 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { cargarArchivo } = require('../controllers/uploads');
-const { validarCampos } = require('../middlewares/validar-campos');
+const { cargarArchivo, actualizarImagen } = require('../controllers/uploads');
+const { coleccionesPermitidas } = require('../helpers');
+const { validarCampos } = require('../middlewares');
 
 const router = Router();
 
 router.post('/', cargarArchivo);
+
+router.put('/:coleccion/:id', [
+    check('id', 'El ID debe ser de mongo').isMongoId(),
+    check('coleccion').custom(c => coleccionesPermitidas( c , ['usuarios', 'productos'])),
+    validarCampos 
+],actualizarImagen)
 
 module.exports = router;
